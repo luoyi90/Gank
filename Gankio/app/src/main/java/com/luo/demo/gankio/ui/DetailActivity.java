42ba98a7284e7a751bf460ed95be5711ff0260f4
luoyi <175262808@qq.com>
2017-05-24 22:59:23 +0800

    update

--- Gankio/app/src/main/java/com/luo/demo/gankio/ui/DetailActivity.java
+++ Gankio/app/src/main/java/com/luo/demo/gankio/ui/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.luo.demo.gankio.ui;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -16,6 +19,7 @@ public class DetailActivity extends AppCompatActivity {
     public static final String MODEL = "model";
     private String mUrl;
     private Android.ResultsBean mResultsBean;
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +41,20 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void initWebView() {
-        WebView mWebView = (WebView) findViewById(R.id.detail_webview);
+        mWebView = (WebView) findViewById(R.id.detail_webview);
         //声明WebSettings子类
         WebSettings webSettings = mWebView.getSettings();
 
         //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
         webSettings.setJavaScriptEnabled(true);
 
+        webSettings.setDomStorageEnabled(true);    //开启DOM形式存储
+        webSettings.setDatabaseEnabled(true);   //开启数据库形式存储
+        String appCacheDir = getApplicationContext().getDir("cache", Context.MODE_PRIVATE).getPath();   //缓存数据的存储地址
+        webSettings.setAppCachePath(appCacheDir);
+        webSettings.setAppCacheEnabled(true);  //开启缓存功能
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);      //缓存模式
+        webSettings.setAllowFileAccess(true);
 
         //设置自适应屏幕，两者合用
         webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
@@ -63,11 +74,27 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.android_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        String tips = "";
         if (item.getItemId() == android.R.id.home) {
             onBackPressed();
             return true;
+        } else if (item.getItemId() == R.id.ac_toolbar_reload) {
+            tips = getResources().getString(R.string.android_detail_reload);
+        } else if (item.getItemId() == R.id.ac_toolbar_copy) {
+            tips = getResources().getString(R.string.android_detail_copy);
+        } else if (item.getItemId() == R.id.ac_toolbar_share) {
+            tips = getResources().getString(R.string.android_detail_share);
+        } else if (item.getItemId() == R.id.ac_toolbar_horizontal) {
+            tips = getResources().getString(R.string.android_detail_horizontal);
         }
+        Snackbar.make(mWebView, tips, Snackbar.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
 }
